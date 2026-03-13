516aa9f32926b0022c2befdb669aac9814c1dbf2
Harini Krishnamoorthy <harinik842@gmail.com>
2026-03-13 15:36:15 +0530

    fix: syntax tokens

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // This allows the build to continue even if there are type errors
     ignoreBuildErrors: true,
   },
-  // If you also want to ignore ESLint warnings during build:
   eslint: {
     ignoreDuringBuilds: true,
   },
